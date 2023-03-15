c8c94a465758596d830deae3d82ca42384158e31
Justin Clark <jclarksummit@gmail.com>
2023-03-15 15:31:42 -0400

    Add User class to models

--- models.py
+++ models.py
@@ -1,1 +1,41 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+    """User."""
+
+    __tablename__ = "users"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    first_name = db.Column(
+        db.String(20),
+        nullable=False
+    )
+
+    # Last name is optional for users
+    last_name = db.Column(
+        db.String(20),
+        nullable=True
+    )
+
+    image_url = db.Column(
+        db.String,
+        nullable=True
+    )
+
